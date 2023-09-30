33de03484b96c9ee63912b73950cf049fd9e7199
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-10-01 03:42:46 +0900

    fix: bug fixed and new symbol detector Feature added

--- train.js
+++ train.js
@@ -1,73 +1,54 @@
-//Alphabet and code
+// Alphabet and code
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
-let code = -100;
+let code = -5;
 
-//DEFINE NEW MESSAGE
-const message = 'HELLO THiS iS ME'; // Should consider Lower and Upper cases
+// Define new message
+const message = "Hello, this is me, Andy!"; // Should consider Lower and Upper cases
 
-//Checking code size
-if(code>alphabet.length || code<alphabet.length*-1){
-    code%=alphabet.length;
-    console.log("code: ",code);
+// Checking code size
+if (code > alphabet.length || code < alphabet.length * -1) {
+  code %= alphabet.length;
+  console.log("code: ", code);
 }
 
-//Decode the Message
-async function decodedMessage(message, code){
-    try{
-        if(typeof(message) !== 'string' || message === ''){
-            throw new Error('Message is empty');
-        }else{
-            result = []
-            for(let word of message.split(' ')){
-                let decodedWord = '';
-                word.split('').filter((letter)=>{
-                    let index = alphabet.indexOf(letter.toLowerCase())+code;
-                    if(index<0){
-                        index += (alphabet.length-1);
-                    }else if(index>(alphabet.length-1)){
-                        index -= (alphabet.length-1);
-                    }
-                    if(letter===letter.toUpperCase()){
-                        decodedWord += alphabet[index].toUpperCase();
-                    }else{
-                        decodedWord += alphabet[index];
-                    }
-                })
-                result.push(decodedWord);
-            }
-            return result.join(' ');
-        }
+// Decode the Message
+function decodedMessage(message, code) {
+  result = [];
+  message.split(" ").forEach((word) => {
+    let decodedWord = "";
+    for (let letter of word) {
+      if (/[^a-zA-Z]/.test(letter)) {
+        decodedWord += letter;
+        continue;
+      }
+      let index = alphabet.indexOf(letter.toLowerCase()) + code;
+      if (index < 0) {
+        index += alphabet.length;
+      } else if (index >= alphabet.length) {
+        index -= alphabet.length;
+      }
+      if (letter === letter.toUpperCase()) {
+        decodedWord += alphabet[index].toUpperCase();
+      } else {
+        decodedWord += alphabet[index];
+      }
     }
-    catch(err){
-        throw err;
-    }
-
+    result.push(decodedWord);
+  });
+  return result.join(" ");
 }
 
-//Encode the Message
-async function encodeMessage(decodedText, code){
-    const message = decodedText;
-    return await decodedMessage(message, code);
-}
- 
-//Creating async Run function
-async function run(){
-    try{
-        const decodedText = await decodedMessage(message, code);
-        console.log('Secret=>', decodedText);
-        setTimeout(async ()=>{
-            code*=-1;
-            const encodedText = await encodeMessage(decodedText, code);
-            console.log('Original=>', encodedText);
-        },5000);
-    }
-    catch(err){
-        throw err;
-    }
-
+// Encode the Message
+function encodeMessage(decodedText, code) {
+  const message = decodedText;
+  return decodedMessage(message, code);
 }
 
-//Run it catch err if it has
-run()
-    .then((result)=>console.log(result))
-    .catch((err)=>console.log(err.message));
+// Creating async Run function
+const decodedText = decodedMessage(message, code);
+console.log("Secret =>", decodedText);
+setTimeout(() => {
+  code *= -1;
+  const encodedText = encodeMessage(decodedText, code);
+  console.log("Original =>", encodedText);
+}, 5000);
